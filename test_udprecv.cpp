2dc8695df9b11247d8698e41999704e10fd73980
KIU Shueng Chuan <nixchuan@gmail.com>
2017-07-19 00:09:41 +0800

    wip: wrap uv_udp_t for ipv4 receive only

---
+++ test_udprecv.cpp
@@ -0,0 +1,27 @@
+#include <memory>
+
+#include <stdio.h>
+
+#include "uvpp_udp.hpp"
+
+int main()
+{
+    uvpp::Loop uvloop;
+
+    uvpp::Signal usignal(uvloop);
+    usignal.set_callback([&uvloop](){
+        uvloop.stop();
+    });
+    usignal.start(SIGINT);
+
+    uvpp::UdpReceiver udprecv(uvloop);
+    udprecv.bind("0.0.0.0:5353", UV_UDP_REUSEADDR);
+    udprecv.join("224.0.0.251", "0.0.0.0");
+    udprecv.set_callback([](char *buf, int len){
+        printf("got %p %d\n", buf, len);
+    });
+    udprecv.start();
+
+    uvloop.run();
+    printf("first loop break\n");
+}
---
+++ uvpp_udp.hpp
@@ -0,0 +1,111 @@
+#ifndef UVPP_UDP_HPP
+#define UVPP_UDP_HPP
+
+#include <string>
+#include <stack>
+
+#include "uvpp.hpp"
+
+namespace uvpp
+{
+    typedef std::function<void(char *, int)> DataReadCallback;
+
+    class UdpReceiver : public BaseHandle<uv_udp_t>
+    {
+    public:
+        UdpReceiver(Loop& uvloop)
+        {
+            uv_udp_init_ex(uvloop, m_handle, AF_INET);
+        }
+
+        ~UdpReceiver() {
+            while (!m_mempool.empty()) {
+                auto ptr = m_mempool.top();
+                m_mempool.pop();
+                free(ptr);
+            }
+        }
+
+        int bind(const std::string& ipaddrport, unsigned int flags) {
+            struct sockaddr_in saddr = { AF_INET };
+            auto colon = ipaddrport.find(':');
+            auto ipaddr = ipaddrport.substr(0, colon);
+            auto portstr = ipaddrport.substr(colon+1);
+            saddr.sin_port = htons(stoi(portstr));
+            inet_aton(ipaddr.c_str(), &saddr.sin_addr);
+            int rc = uv_udp_bind(m_handle, (struct sockaddr*)&saddr, flags);
+            assert(rc==0 || print_error(rc));
+            return rc;
+        }
+
+        int join(const std::string& mcast_addr, const std::string& iface_addr) {
+            int rc =  uv_udp_set_membership(m_handle, mcast_addr.c_str(),
+                iface_addr.c_str(), UV_JOIN_GROUP);
+            assert(rc==0 || print_error(rc));
+            return rc;
+        }
+
+        void set_callback(DataReadCallback cb)
+        {
+            m_callback = cb;
+        }
+
+        void start()
+        {
+            int rc = uv_udp_recv_start(m_handle,
+                [](uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf){
+                    auto self = static_cast<UdpReceiver*>(handle->data);
+                    self->alloc_callback(suggested_size, buf);
+                },
+                [](uv_udp_t *handle, ssize_t nread, const uv_buf_t *buf,
+                    const struct sockaddr *addr, unsigned flags){
+                    auto self = static_cast<UdpReceiver*>(handle->data);
+                    self->recv_callback(nread, buf, (const struct sockaddr_in *)addr,
+                        flags);
+                });
+            assert(rc==0 || print_error(rc));
+        }
+
+        void stop()
+        {
+            int rc = uv_udp_recv_stop(m_handle);
+            assert(rc==0 || print_error(rc));
+        }
+
+    private:
+        void alloc_callback(size_t suggested_size, uv_buf_t *buf) {
+            if (m_mempool.empty()) {
+                buf->base = (char *)malloc(suggested_size);
+                buf->len = suggested_size;
+            }
+            else {
+                buf->base = (char *)m_mempool.top();
+                buf->len = suggested_size;
+                m_mempool.pop();
+            }
+        }
+
+        void recv_callback(ssize_t nread, const uv_buf_t *buf,
+            const struct sockaddr_in *addr, unsigned flags) {
+            if (nread==0 && addr==NULL) {
+                // EAGAIN: don't pass this to user
+            }
+            else if (nread < 0) {
+                m_callback(buf->base, nread);
+            }
+            else {
+                m_callback(buf->base, nread);
+            }
+
+            if (buf->base)
+                m_mempool.push(buf->base);
+        }
+
+    private:
+        DataReadCallback m_callback;
+        std::stack<void*> m_mempool;
+    };
+}
+
+#endif
+
