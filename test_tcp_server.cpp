5a3b4c1120e8957a8447f6254a16442a3528f0ea
KIU Shueng Chuan <nixchuan@gmail.com>
2017-11-09 23:30:33 +0800

    no need to wrap in unique_ptr
    we had wrapped in unique_ptr because we were previously using the
    pointer value as a lookup key.
    
    we don't need a lookup key anymore since we now hold the iterator.

--- test_tcp_server.cpp
+++ test_tcp_server.cpp
@@ -1,7 +1,5 @@
 #include <list>
-#include <memory>
 #include <string>
-#include <algorithm>
 
 #include "uvpp_tcp.hpp"
 
@@ -17,21 +15,21 @@ int main()
 
     uvpp::Tcp server(uvloop);
 
-    std::list<std::unique_ptr<uvpp::Tcp>> clients;
+    std::list<uvpp::Tcp> clients;
 
     struct sockaddr_in saddr;
     uv_ip4_addr("127.0.0.1", 12345, &saddr);
     server.bind(saddr);
-    server.listen([&](uvpp::Tcp client){
+    server.listen([&](uvpp::Tcp conn){
         struct sockaddr_in saddr;
-        client.getpeername(saddr);
+        conn.getpeername(saddr);
         char buffer[64];
         uv_ip4_name(&saddr, buffer, sizeof(buffer));
         printf("connection from %s:%d\n", buffer, ntohs(saddr.sin_port));
 
-        clients.emplace_back(new uvpp::Tcp(std::move(client)));
+        clients.emplace_back(std::move(conn));
         auto connit = --clients.end();
-        (*connit)->set_callback([&clients, connit](char *buf, int len){
+        connit->set_callback([&clients, connit](char *buf, int len){
             if (len < 0) {
                 uvpp::print_error(len);
                 clients.erase(connit);
@@ -41,9 +39,9 @@ int main()
             auto str = std::string(buf, len);
             std::cout << str << std::endl;
 
-            (*connit)->write(buf, len);
+            connit->write(buf, len);
         });
-        (*connit)->read_start();
+        connit->read_start();
     });
 
     uvloop.run();
